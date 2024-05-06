31386f54a9716b92c43087489137fcb9c163af2a
Kianne Bulatao <167046743+krcbulatao@users.noreply.github.com>
2024-05-06 11:59:23 +0800

    Delete Bulatao_B2_answer directory

--- Bulatao_B2_answer/bulatao_b2.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df=pd.read_csv("/mnt/c/Users/krcbu/Downloads/Exam_Table.csv")
-
-filtered_df=df[df['Genus'].str.casefold().str.startswith('st', na=False)]
-
-filtered_df.to_csv("/mnt/c/Users/krcbu/Desktop/bio 191/Bulatao_B2_answer/b2_output2.csv", index=False)
